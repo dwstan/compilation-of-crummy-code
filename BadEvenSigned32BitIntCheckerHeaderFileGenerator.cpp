b7af2fb2322cc0d97f37fe1c0428518601a59883
dstan <64676122+dwstan@users.noreply.github.com>
2022-03-05 20:36:44 -0800

    Create BadEvenSigned32BitIntCheckerHeaderFileGenerator.cpp
    it do

---
+++ BadEvenSigned32BitIntCheckerHeaderFileGenerator.cpp
@@ -0,0 +1,37 @@
+/*
+####################################################
+####################################################
+## warning this code will make a 90 gb h file lol ##
+####################################################
+####################################################
+*/
+
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream fout;
+    fout.open("isEven.h");
+
+    fout << "\#ifndef IsEven_h\n"
+        << "\#define IsEven_h\n\n"
+        << "bool isEven(int number)\n"
+        << "{\n";
+
+    for (int count = -2147483648; count < 2147483647; count++)
+    {
+        if (count % 2 == 0)
+        {
+            fout << "if(number == " << count << ")\n"
+                << "\treturn true\;\n";
+        }
+    }
+
+    fout << "return false;\n";
+    fout << "}\n"
+        << "\#endif";
+
+    fout.close();
+    return 0;
+}
